4410f453bf59f11396519e9bd4a95d498ac051a5
LeonardoSalmento <Salmento_@hotmail.com>
2021-01-22 17:25:25 -0300

    Adicionando exceptions de conflit e bad request

---
+++ src/main/java/com/example/bankProject/exception/ConflictException.java
@@ -0,0 +1,13 @@
+package com.example.bankProject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.CONFLICT)
+public class ConflictException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public ConflictException(String message){
+        super(message);
+    }
+}
